8bc2f0656c7396c5770f00c3d5997db4ac6774df
JIbald <45597910+JIbald@users.noreply.github.com>
2020-08-03 11:53:18 +0200

    added camera-shake on projectile impact, and a bigger camera-shake when either the tank or a turret explode/dies

--- Source/ToonTanks/Actors/ProjectileBase.h
+++ Source/ToonTanks/Actors/ProjectileBase.h
@@ -34,6 +34,9 @@ private:
 	USoundBase* HitSound;
 	UPROPERTY(EditAnywhere, Category = "Effects")
 	USoundBase* LaunchSound;
+	UPROPERTY(EditAnywhere, Category = "Effects")
+	TSubclassOf<UCameraShake> HitShake;
+
 
 	// Functions
 	UFUNCTION()
